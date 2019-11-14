ace4ad16aa4d2a3674d027ef00606bcf18b43e67
rencw <627127819@qq.com>
2019-11-14 11:50:38 +0800

    commit

---
+++ xgg-auth-oauth2-resource/src/main/java/xgg/auth/oauth2/resource/XggAuthOauth2ResourceApplication.java
@@ -0,0 +1,13 @@
+package xgg.auth.oauth2.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XggAuthOauth2ResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XggAuthOauth2ResourceApplication.class, args);
+    }
+
+}
